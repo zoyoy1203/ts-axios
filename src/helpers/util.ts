71e9af586e856e501b1248497bbff9a066212c5d
zoyoy1203 <52310049+zoyoy1203@users.noreply.github.com>
2020-02-29 17:11:41 +0800

    处理请求body并测试

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,3 +1,5 @@
+import { fuchsia } from "color-name";
+
 const toString = Object.prototype.toString
 
 // 判断参数类型
@@ -5,6 +7,11 @@ export function isDate(val: any): val is Date {
     return toString.call(val) === '[object Date]'
 }
 
-export function isObject(val: any): val is Object {
-    return val !== null && typeof val === 'object'
+// export function isObject(val: any): val is Object {
+//     return val !== null && typeof val === 'object'
+// }
+
+// 判断是否是普通对象
+export function isPlainObject(val: any): val is Object {
+    return toString.call(val) === '[object Object]'
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
-
+import { transformRequest } from './helpers/data'
 function axios (config: AxiosRequestConfig) {
     processConfig(config)
     xhr(config)
@@ -11,11 +11,17 @@ function axios (config: AxiosRequestConfig) {
 function processConfig(config: AxiosRequestConfig): void {
     // 对url进行一些处理
     config.url = transformURL(config)
+    // 对data进行一些处理
+    config.data = transformRequestData(config)
 }
 
 function transformURL(config: AxiosRequestConfig): string {
     const { url, params } = config
     return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any{
+    return transformRequest(config.data)
 }
 
 export default axios

28371a857c7c343de5724dbea836a96c8a112267
zoyoy1203 <52310049+zoyoy1203@users.noreply.github.com>
2020-03-02 00:18:16 +0800

    函数重载完成

--- src/types/index.ts
+++ src/types/index.ts
@@ -57,4 +57,6 @@ export interface Axios {
 
 export interface AxiosInstance extends Axios{
     (config: AxiosRequestConfig): AxiosPromise
+
+    (url: string, config?: AxiosRequestConfig): AxiosPromise
 }
